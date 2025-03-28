c7845491cf179ae3dd17cb178eefc146ee012e6a
anishachanti <89964565+anishachanti@users.noreply.github.com>
2025-03-28 23:09:41 +0530

    Update EmployeeRepository.java

--- visa-backend/demo/src/main/java/com/visa/demo/repository/EmployeeRepository.java
+++ visa-backend/demo/src/main/java/com/visa/demo/repository/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.visa.demo.repository;
 
 import com.visa.demo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 import java.util.List;
 
@@ -10,8 +13,13 @@ public interface EmployeeRepository extends JpaRepository<Employee, String> {
     List<Employee> findByReportingManagerId(String reportingManagerId);
 
     boolean existsByReportingManagerId(String reportingManagerId);
-}
 
+    @Query("SELECT e FROM Employee e WHERE e.role = :role")
+    List<Employee> findByRole(@Param("role") String role);
+
+
+
+}
 
 
 

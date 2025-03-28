7021c3f58dd2892d212487ed83d0ba81d42318df
anishachanti <89964565+anishachanti@users.noreply.github.com>
2025-03-28 23:10:44 +0530

    Update VisaApplicationRepository.java

--- visa-backend/demo/src/main/java/com/visa/demo/repository/VisaApplicationRepository.java
+++ visa-backend/demo/src/main/java/com/visa/demo/repository/VisaApplicationRepository.java
@@ -2,6 +2,7 @@ package com.visa.demo.repository;
 
 import com.visa.demo.model.VisaApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,7 +14,28 @@ public interface VisaApplicationRepository extends JpaRepository<VisaApplication
     // ✅ Fetch all PENDING applications assigned to a specific reporting manager
     @Query("SELECT v FROM VisaApplication v JOIN Employee e ON v.empId = e.empId WHERE e.reportingManagerId = :managerId AND v.status = 'PENDING'")
     List<VisaApplication> findByManagerId(String managerId);
+
+    // ✅ Search applications by Employee ID (Partial Match Allowed)
+    @Query("SELECT v FROM VisaApplication v WHERE v.empId LIKE %:empId%")
+     List<VisaApplication> searchByEmployeeId(@Param("empId")String empId);
+
+   /* @Query("SELECT v FROM VisaApplication v WHERE v.status = 'APPROVED'")
+    List<VisaApplication> findApprovedApplications();*/
+
+    /*@Query("SELECT v FROM VisaApplication v WHERE v.status = 'APPROVED' ORDER BY v.dateApproved DESC")
+    List<VisaApplication> findApprovedApplications();*/
+
+    /*@Query("SELECT v FROM VisaApplication v WHERE v.status != 'REJECTED'")
+    List<VisaApplication> findInProgressApplications();
+
+     */
+
+    @Query("SELECT v FROM VisaApplication v WHERE v.status IN ('Approved', 'Petition Submitted', 'Petition Approved','Petition Rejected')")
+    List<VisaApplication> findInProgressApplications();
+
+
+
+
 }
 
- 
 
